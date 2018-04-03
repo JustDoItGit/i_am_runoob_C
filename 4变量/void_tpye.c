d9cf89d7fb11bace9305a30605509e4101e3e5f3
coolcumt <cool_kj@126.com>
2018-04-03 14:54:36 +0800

    c语言变量

---
+++ 4变量/void_tpye.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 变量申明
+extern int a, b;
+extern int c;
+extern float f;
+
+int main()
+{
+	/* 变量定义 */
+	int a, b;
+	int c;
+	float f;
+
+	/* 初始化 */
+	a = 10;
+	b = 20;
+	c = a + b;
+	printf("value of c: %d \n", c);
+
+	f = 70.0/3.0;
+	printf("value of f: %f \n", f);
+
+	return 0;
+}
