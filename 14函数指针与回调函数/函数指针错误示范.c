03b8460ddb2a0c4937507340af5e78dfc2b83836
coolcumt <cool_kj@126.com>
2018-07-05 00:38:45 +0800

    函数指针与回调函数

---
+++ 14函数指针与回调函数/函数指针错误示范.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int max(int x, int y)
+{
+	return x > y ? x:y;
+}
+/* 注意是函数声明指针指向函数地址，并不能直接声明指针函数 */
+int (*p)(int x, int y)
+{
+	return x > y ? x:y;
+}
+int main(int argc, char const *argv[])
+{
+	/* p 是指针函数 */
+	/* int(*p)(int, int) = &max; */
+	int a,b,c,d;
+
+	printf("请输入三个字：\n");
+	scanf("%d %d %d", &a,&b,&c);
+
+	/* 与直接调用函数等价，d = max(max(a, b), c) */
+	d = p(p(a,b),c);
+	printf("最大的数字是：%d\n", d);
+	return 0;
+}
