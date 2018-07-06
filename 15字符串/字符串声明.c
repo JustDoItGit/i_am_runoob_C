5dffde8ec923ae158e29f55be9ca38f47e18b086
coolcumt <cool_kj@126.com>
2018-07-07 00:05:09 +0800

    字符串

---
+++ 15字符串/字符串声明.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+	char yellow[] = "hello";
+	printf("%s\n", greeting);
+	printf("%s\n", yellow);
+	return 0;
+}
+
