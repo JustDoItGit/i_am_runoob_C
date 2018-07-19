18abd80ba95b84be56715ed1cb5a0269e1ef8d1d
coolcumt <cool_kj@126.com>
2018-07-19 23:58:37 +0800

    算数转换

---
+++ 23强制类型转换/算数转换.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 17;
+    char c = 'c';
+    float sum;
+    sum = i + c;
+    printf("Value of sum d : %d\n", sum);
+    printf("Value of sum f : %f\n", sum);
+}
